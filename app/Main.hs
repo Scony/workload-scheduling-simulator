afb74f81ad097281565d4642436b2e69d2708f49
Pawel Lampe <pawel.lampe@gmail.com>
2019-01-11 21:39:44 +0100

    hs - simple validation of solution

--- app/Main.hs
+++ app/Main.hs
@@ -11,6 +11,7 @@ import Solution (totalFlow, costs, flow)
 import Schedule (calculateSolution)
 import QueueAlgorithms (so)
 import Job (arrival)
+import Validator (validateSolution)
 
 replace a b s = map (\x -> if x == a then b else x) s
 mkLines x = map (replace '_' ' ') $ words $ replace ' ' '_' x
@@ -33,8 +34,12 @@ main = do
   hPutStrLn stderr $ "> operations: " ++ show operationsNum
   hPutStrLn stderr $ "> algorithm: " ++ algorithm
 
-  let scheduleAlgorithm alg = costs jobs (calculateSolution jobs $ alg jobs operations machines) flow
-  let queueAlgorithm alg = costs jobs (alg jobs operations machines) flow
+  let scheduleAlgorithm alg = costs jobs solution flow
+        where solution = validateSolution jobs operations
+                         $ calculateSolution jobs
+                         $ alg jobs operations machines
+  let queueAlgorithm alg = costs jobs solution flow
+        where solution = validateSolution jobs operations $ alg jobs operations machines
   let cjsInOrder cjs = sortBy (\(_, j1) (_, j2) -> compare (arrival j1) (arrival j2)) cjs
   let jobFlows cjs = mapM_ (\(c, _) -> putStrLn $ show c) (cjsInOrder cjs)
 
---
+++ src/Validator.hs
@@ -0,0 +1,37 @@
+module Validator
+  ( validateSolution
+  ) where
+
+import Data.List.Extra (nubOrdBy)
+
+import Assignment (Assignment, finish, operation)
+import Job (Job, arrival)
+import Operation (Operation, duration, parentOf, uuid)
+
+assert pred msg x = if pred then x else error msg
+
+validateSolution :: [Job] -> [Operation] -> [Assignment] -> [Assignment]
+validateSolution js ops as = validateSolutionSize ops
+                             $ validateEachMachineProcessAtMostOneOperationAtTheTime
+                             $ validateSingularOperationExecutions ops
+                             $ validateOperationFinishes js as
+
+validateSolutionSize :: [Operation] -> [Assignment] -> [Assignment]
+validateSolutionSize ops as = assert (length ops == length as) msg as
+  where msg = "Solution size is invalid"
+
+validateOperationFinishes :: [Job] -> [Assignment] -> [Assignment]
+validateOperationFinishes js as = assert (all operationFinishIsValid as) msg as
+  where operationFinishIsValid a = finish a
+                                   >=
+                                   (arrival $ parentOf js $ operation a) + (duration $ operation a)
+        msg = "Operation finishes are not valid"
+
+validateSingularOperationExecutions :: [Operation] -> [Assignment] -> [Assignment]
+validateSingularOperationExecutions ops as = assert (length ops == length uniqueAs) msg as
+  where uniqueAs = nubOrdBy (\a b -> compare (uuid $ operation a) (uuid $ operation b)) as
+        msg = "Operation executions are not singular: "
+
+validateEachMachineProcessAtMostOneOperationAtTheTime :: [Assignment] -> [Assignment]
+validateEachMachineProcessAtMostOneOperationAtTheTime as = assert True msg as
+  where msg = ""
